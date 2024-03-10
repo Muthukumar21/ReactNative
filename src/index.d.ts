cb0f0154cbccef6324164a15fd9fedb4e00d08c5
muthukumar.m <muthukumar.m@newgensoft.com>
2024-03-10 17:36:44 +0530

    currency app

---
+++ src/index.d.ts
@@ -0,0 +1,12 @@
+interface Currency {
+    name: string,
+    value: number,
+    flag: string,
+    symbol: string,
+}
+
+interface SnackbarOptions {
+    text: string;
+    duration: number;
+    // Add more properties as needed
+  }
