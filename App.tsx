0619150bb852320f7ca983e40d95ad3d7f297c18
muthukumar.m <muthukumar.m@newgensoft.com>
2024-03-10 14:30:14 +0530

    new commit for the currencychange

--- App.tsx
+++ App.tsx
@@ -1,48 +1,14 @@
-import React, { useState } from 'react';
-import { Button, SafeAreaView, StyleSheet, Text, View, useColorScheme } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
 
-const style = StyleSheet.create({
-  container:{
-    height:"100%",
-    display:"flex",
-    justifyContent:"center",
-    alignItems:"center",
-  },
-  darkMode:{
-    backgroundColor:"black"
-  },
-  lightMode:{
-    backgroundColor:"white"
-  },
-  red:{
-    color:"red"
-  },
-  blue:{
-    color:"green"
-  }
-})
-
-function App(){
-  const [changeColor,setChangeColor] = useState('dark')
-  const onChange = (ref:string)=>{
-    setChangeColor(ref);
-  }
-  const isDark = useColorScheme() == changeColor
-  return(
-    <SafeAreaView>
-    <View style={[style.container,isDark?style.lightMode:style.darkMode]}>
-      <Text style={style.blue}>Raji</Text>
-      <Text style={style.red}>Muthu</Text>
-      <Button
-      onPress={()=>onChange(changeColor === 'dark' ? "light":"dark")}
-      title="Change Color"
-      color="#841584"
-      accessibilityLabel="Learn more about this purple button"
-      />
+const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
     </View>
-    </SafeAreaView>
-  );
-
+  )
 }
 
-export default App;
+export default App
+
+const styles = StyleSheet.create({})
